710b3729b2cef3736f735219143e79263fb41d3b
Yair Chuchem <yairchu@gmail.com>
2023-09-05 14:47:44 +0300

    Cloudflare pages integration attempt

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,10 +3,18 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
 		// interface PageData {}
-		// interface Platform {}
+		interface Locals {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+		}
 	}
 }
 
-export {};
+export { };
