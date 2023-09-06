fcd82d1e1237b70e80bb0529b684399321c8eae8
Yair Chuchem <yairchu@gmail.com>
2023-09-06 10:35:34 +0300

    Invite folks to help me when they finish the game

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -147,6 +147,14 @@
 				{/if}
 			</p>
 		</button>
+	{:else}
+		<p>
+			This is all I have for now, this game is in very early alpha stages. I'm not a web or game
+			developer, nor UX designer or graphics artist. If you would like to help make it, your help is
+			very wanted and appreciated! Development is done over at <a
+				href="https://github.com/yairchu/soul-arithmetic">GitHub</a
+			>. Cheers! - Yair
+		</p>
 	{/if}
 </div>
 
