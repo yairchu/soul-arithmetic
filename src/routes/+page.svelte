36954ec074bbe6ed3f9abe6212e8e081922d0851
Yair Chuchem <yairchu@gmail.com>
2023-09-12 13:51:43 +0300

    Extract questions to separate module

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,111 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-
-    function shuffle(arr: any[]) {
-        return arr
-            .map((value: any) => ({ value, key: Math.random() }))
-            .sort((a: { key: number }, b: { key: number }) => a.key - b.key)
-            .map(({ value }) => value);
-    }
-
-    let questions = shuffle([
-        {
-            title: 'Elves and Dwarves',
-            image: 'high-elf-jurum-ka.jpg',
-            quote: 'The elven army should wipe Nogrod* off the face of middle-earth',
-            info: '- Jurum Ka, high elven minister of weather manipulation. (Nogrod is a dwarvish town)',
-            origin: 'The Israeli finance minister on an Arab village',
-            origin_image: 'bezalel-smotrich.jpg',
-            origin_info: /*html*/ `
-                <p>
-                    Israel's finance minister, Bezalel Smotrich, called for the destruction of Huwara, a West
-                    Bank Palestinian village days after
-                    <a href="https://en.wikipedia.org/wiki/Huwara_rampage">
-                        it was attacked by a mob of settlers who torched dozens of homes and killed one
-                        resident</a>.
-                </p>
-                <p>
-                    Smotrich: “The village of Hawara needs to be wiped out. I think that the State of Israel
-                    needs to do that - not, God forbid, private individuals”
-                </p>
-                `,
-            alt_answers: [
-                'The leader of an Arab terror organization on an Israeli settlement',
-                'A Russian general on a Ukranian city',
-                'A Nazi affiliated journalist in 1930s Germany on a Jewish suburb'
-            ]
-        },
-        {
-            title: 'Merfolk and humans',
-            image: 'mermen-king-arion.jpg',
-            quote: 'I would prefer my daughter to die than have a relationship with a surface dweller',
-            info: '- Arion, king of the merfolk. ("Surface dweller" is a slur for humans)',
-            origin: 'The president of Brazil on homosexuality',
-            origin_image: 'jair-bolsonaro.jpg',
-            origin_info: /*html*/ `
-                <p>
-                    Jair Bolsonaro, the president of Brazil between 2019-2022
-                    <a href="https://en.wikipedia.org/wiki/Jair_Bolsonaro#Views_on_homosexuality">
-                        has said that if he had a gay son then he would prefer for him to die in an accident</a>.
-                </p>
-                `,
-            alt_answers: [
-                'A Mormon apostle on crossbreeding with Jews',
-                'The Israeli minister of transportation on mixed marriages',
-                'A US senator on relationships with Republicans'
-            ]
-        },
-        {
-            title: 'Magicians and muggles',
-            image: 'magic-councilour-sir-bauldinium.jpg',
-            quote: 'Magicians and muggles should not mix, it only leads to tension and violence',
-            info: '- Sir Bauldinium, member of the magic council (muggles are non-magical humans)',
-            origin: 'The prime minister of South Africa during Apartheid',
-            origin_image: 'hendrik-verwoerd.jpg',
-            origin_info: /*html*/ `
-                <p>
-                    Hendrik Verwoerd, the founding prime minister of Apartheid South Africa,
-                    <a href="https://en.wikiquote.org/wiki/Hendrik_Verwoerd">has said</a>:
-                </p>
-                <p>
-                    "There will be competition and conflict everywhere.
-                    So long as the points of contact are still comparatively few, as is the case now,
-                    friction and conflict will be few and less evident.
-                    The more this intermixing develops, however, the stronger the conflict will become."
-                </p>
-                `,
-            alt_answers: [
-                'The prime minister of Japan on Koreans',
-                'The chief Rabbi of Israel on mixed marriages',
-                'The president of the US on Mexicans'
-            ]
-        },
-        {
-            title: 'Cyborgs and humans',
-            image: 'professor-zoltar.jpg',
-            quote: 'Bios should not be accepted into universities, they are too stupid to learn',
-            info: '- Professor Zoltar, head of the first Mars university (bios are non-cyborg humans)',
-            origin: 'The Israeli chief Rabbi on women studying Torah',
-            origin_image: 'ovadia-yosef.jpg',
-            origin_info: /*html*/ `
-                <p>
-                    Rabbi Ovadia Yosef
-                    <a href="https://www.jpost.com/jewish-world/jewish-news/rabbi-ovadia-women-should-stick-to-cooking-sewing">
-                        has said</a>
-                    that women should not engage in religious studies because they are stupid.
-                </p>
-                `,
-            alt_answers: [
-                'The president of the US in the beginning of the 19th century on people of color',
-                'The president of Oxford university on women studying engineering',
-                'The leader of ISIS on women participating in paramilitary organizations'
-            ]
-        }
-    ]);
-    questions.forEach((question) => {
-        question.answers = shuffle(question.alt_answers.concat([question.origin]));
-        question.selected = [];
-    });
+    import { questions } from './questions';
 
     let score = 0;
     let cur_question_idx = 0;
@@ -162,8 +57,8 @@
         homophobia, and other forms of intolerance.
     </p>
     <p>
-        It consists of imaginary offensive quotes which are based on actual quotes said by actual people
-        of importance and positions of power in our society.
+        It consists of imaginary offensive quotes which are based on actual quotes said by actual
+        people of importance and positions of power in our society.
     </p>
     <p>If that's fine by you, then here we go:</p>
     <hr />
@@ -178,8 +73,8 @@
             <h2>"{question.quote}"</h2>
             <p>{question.info}</p>
             <p>
-                The above statement is based on a statement originally said by a human on other humans.
-                Guess who it was:
+                The above statement is based on a statement originally said by a human on other
+                humans. Guess who it was:
             </p>
             {#each question.answers as answer}
                 <label class:origin={question_idx < cur_question_idx && answer == question.origin}>
@@ -200,7 +95,10 @@
                 </div>
                 <p>Score: <b>{question.score_after}</b></p>
                 {#if question_idx < questions.length - 1}
-                    <button on:click={() => scrollIntoView(`question-${question_idx + 1}`)} class="next">
+                    <button
+                        on:click={() => scrollIntoView(`question-${question_idx + 1}`)}
+                        class="next"
+                    >
                         Next question
                     </button>
                 {/if}
@@ -222,9 +120,9 @@
         </button>
     {:else}
         <p>
-            This is all I have for now, this game is in very early alpha stages. I'm not a web or game
-            developer, nor UX designer or graphics artist. If you would like to help make it, your help is
-            very wanted and appreciated! Development is done over at <a
+            This is all I have for now, this game is in very early alpha stages. I'm not a web or
+            game developer, nor UX designer or graphics artist. If you would like to help make it,
+            your help is very wanted and appreciated! Development is done over at <a
                 href="https://github.com/yairchu/soul-arithmetic">GitHub</a
             >. Cheers! - Yair
         </p>
---
+++ src/routes/questions.ts
@@ -0,0 +1,105 @@
+function shuffle(arr: any[]) {
+    return arr
+        .map((value: any) => ({ value, key: Math.random() }))
+        .sort((a: { key: number }, b: { key: number }) => a.key - b.key)
+        .map(({ value }) => value);
+}
+
+export let questions = shuffle([
+    {
+        title: 'Elves and Dwarves',
+        image: 'high-elf-jurum-ka.jpg',
+        quote: 'The elven army should wipe Nogrod* off the face of middle-earth',
+        info: '- Jurum Ka, high elven minister of weather manipulation. (Nogrod is a dwarvish town)',
+        origin: 'The Israeli finance minister on an Arab village',
+        origin_image: 'bezalel-smotrich.jpg',
+        origin_info: /*html*/ `
+            <p>
+                Israel's finance minister, Bezalel Smotrich, called for the destruction of Huwara, a West
+                Bank Palestinian village days after
+                <a href="https://en.wikipedia.org/wiki/Huwara_rampage">
+                    it was attacked by a mob of settlers who torched dozens of homes and killed one
+                    resident</a>.
+            </p>
+            <p>
+                Smotrich: “The village of Hawara needs to be wiped out. I think that the State of Israel
+                needs to do that - not, God forbid, private individuals”
+            </p>
+            `,
+        alt_answers: [
+            'The leader of an Arab terror organization on an Israeli settlement',
+            'A Russian general on a Ukranian city',
+            'A Nazi affiliated journalist in 1930s Germany on a Jewish suburb'
+        ]
+    },
+    {
+        title: 'Merfolk and humans',
+        image: 'mermen-king-arion.jpg',
+        quote: 'I would prefer my daughter to die than have a relationship with a surface dweller',
+        info: '- Arion, king of the merfolk. ("Surface dweller" is a slur for humans)',
+        origin: 'The president of Brazil on homosexuality',
+        origin_image: 'jair-bolsonaro.jpg',
+        origin_info: /*html*/ `
+            <p>
+                Jair Bolsonaro, the president of Brazil between 2019-2022
+                <a href="https://en.wikipedia.org/wiki/Jair_Bolsonaro#Views_on_homosexuality">
+                    has said that if he had a gay son then he would prefer for him to die in an accident</a>.
+            </p>
+            `,
+        alt_answers: [
+            'A Mormon apostle on crossbreeding with Jews',
+            'The Israeli minister of transportation on mixed marriages',
+            'A US senator on relationships with Republicans'
+        ]
+    },
+    {
+        title: 'Magicians and muggles',
+        image: 'magic-councilour-sir-bauldinium.jpg',
+        quote: 'Magicians and muggles should not mix, it only leads to tension and violence',
+        info: '- Sir Bauldinium, member of the magic council (muggles are non-magical humans)',
+        origin: 'The prime minister of South Africa during Apartheid',
+        origin_image: 'hendrik-verwoerd.jpg',
+        origin_info: /*html*/ `
+            <p>
+                Hendrik Verwoerd, the founding prime minister of Apartheid South Africa,
+                <a href="https://en.wikiquote.org/wiki/Hendrik_Verwoerd">has said</a>:
+            </p>
+            <p>
+                "There will be competition and conflict everywhere.
+                So long as the points of contact are still comparatively few, as is the case now,
+                friction and conflict will be few and less evident.
+                The more this intermixing develops, however, the stronger the conflict will become."
+            </p>
+            `,
+        alt_answers: [
+            'The prime minister of Japan on Koreans',
+            'The chief Rabbi of Israel on mixed marriages',
+            'The president of the US on Mexicans'
+        ]
+    },
+    {
+        title: 'Cyborgs and humans',
+        image: 'professor-zoltar.jpg',
+        quote: 'Bios should not be accepted into universities, they are too stupid to learn',
+        info: '- Professor Zoltar, head of the first Mars university (bios are non-cyborg humans)',
+        origin: 'The Israeli chief Rabbi on women studying Torah',
+        origin_image: 'ovadia-yosef.jpg',
+        origin_info: /*html*/ `
+            <p>
+                Rabbi Ovadia Yosef
+                <a href="https://www.jpost.com/jewish-world/jewish-news/rabbi-ovadia-women-should-stick-to-cooking-sewing">
+                    has said</a>
+                that women should not engage in religious studies because they are stupid.
+            </p>
+            `,
+        alt_answers: [
+            'The president of the US in the beginning of the 19th century on people of color',
+            'The president of Oxford university on women studying engineering',
+            'The leader of ISIS on women participating in paramilitary organizations'
+        ]
+    }
+]);
+questions.forEach((question) => {
+    question.answers = shuffle(question.alt_answers.concat([question.origin]));
+    question.selected = [];
+});
