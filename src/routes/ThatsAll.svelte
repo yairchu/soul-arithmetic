ece2f23aaf193415358a2cd5ce3446b5991a7c7c
Yair Chuchem <yairchu@gmail.com>
2023-09-19 15:20:15 +0300

    Auto formatting

--- src/routes/ThatsAll.svelte
+++ src/routes/ThatsAll.svelte
@@ -1,7 +1,7 @@
 <p>
-    This is all I have for now, this game is in very early alpha stages. I'm not a web or
-    game developer, nor UX designer or graphics artist. If you would like to help make it,
-    your help is very wanted and appreciated! Development is done over at <a
+    This is all I have for now, this game is in very early alpha stages. I'm not a web or game
+    developer, nor UX designer or graphics artist. If you would like to help make it, your help is
+    very wanted and appreciated! Development is done over at <a
         href="https://github.com/yairchu/soul-arithmetic">GitHub</a
     >. Cheers! - Yair
 </p>
