5460ddfddbc7b9dac933c0ea7dc805ee737dfed5
Yair Chuchem <yairchu@gmail.com>
2023-09-20 13:00:01 +0300

    Formatting using "npx prettier -w ."

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,20 +1,20 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface PageData {}
-		interface Locals {}
-		interface Platform {
-			env: {
-				COUNTER: DurableObjectNamespace;
-			};
-			context: {
-				waitUntil(promise: Promise<any>): void;
-			};
-			caches: CacheStorage & { default: Cache }
-		}
-	}
+    namespace App {
+        // interface Error {}
+        // interface PageData {}
+        interface Locals {}
+        interface Platform {
+            env: {
+                COUNTER: DurableObjectNamespace;
+            };
+            context: {
+                waitUntil(promise: Promise<any>): void;
+            };
+            caches: CacheStorage & { default: Cache };
+        }
+    }
 }
 
-export { };
+export {};
--- src/lib/scroll.ts
+++ src/lib/scroll.ts
@@ -24,6 +24,6 @@ export function onScrollStop(action: () => void) {
             lastPos = pos;
             requestAnimationFrame(checkPos);
         }
-    }
+    };
     requestAnimationFrame(checkPos);
 }
